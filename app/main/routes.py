0cce79742dd8cbbac1e2c01a739e1fa80e93e524
huaise <huaise201@sina.com>
2017-09-08 18:09:54 +0800

    add post page

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,12 @@
 from flask import render_template, url_for
 from . import main
 
-@main.route('/', methods = ['GET', 'POST'])
+@main.route('/', methods = ['GET'])
 def index():
 	return render_template('main/index.html')
+
+@main.route('/home', methods = ['GET'])	
+def home():
+
+	
+	return render_template('main/home.html')
